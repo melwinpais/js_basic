7ce03c7e00a4e4c884682d39188e64e5a98f7a73
mpais <mpais@ebay.com>
2019-01-14 10:31:53 +1100

    Update on exercise

--- 06_02/script.js
+++ 06_02/script.js
@@ -1,3 +1,22 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+function runClock() {
+  var date = new Date();
+  let hr = date.getHours() % 12;
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log(" Time is " + hr + ":" + min + ":" + sec + ":")
+
+  let hrPosition = (hr + (min === 0 ? 0 : min /60)) * (360/12);
+  let minPosition = (min + (sec === 0 ? 0 : sec/60)) * (360/60);
+  let secPosition = sec * (360/60);
+
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
+}
+
+
+var interval = setInterval(runClock, 1000);
